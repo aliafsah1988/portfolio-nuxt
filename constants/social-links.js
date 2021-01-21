ab638152b5ad29a5703c1e5f6fd8df293d855d2a
Ali Afsahnoudeh <ali.afsah1988@gmail.com>
2021-01-21 20:47:23 +0100

    social links updated

--- constants/social-links.js
+++ constants/social-links.js
@@ -1,4 +1,6 @@
 export default {
-  LINKEDIN: 'https://www.linkedin.com/in/arezoo-goshtasbi/',
-  GITHUB: 'https://github.com/ArezooGoshtasbi',
+  LINKEDIN: 'https://www.linkedin.com/in/aliafsahnoudeh/',
+  GITHUB: 'https://github.com/aliafsahnoudeh',
+  MEDIUM: 'https://medium.com/@aliafsah1988',
+  FACEBOOK: 'https://www.facebook.com/profile.php?id=100001857186844',
 }
