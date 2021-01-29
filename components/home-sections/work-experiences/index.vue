17f3783aa451ea390362063b2145f3e8463ac190
Ali Afsahnoudeh <ali.afsah1988@gmail.com>
2021-01-29 12:26:15 +0100

    fields images added

--- components/home-sections/work-experiences/index.vue
+++ components/home-sections/work-experiences/index.vue
@@ -9,26 +9,34 @@
         <FieldsItem
           :title="$t('fields.front-end.title')"
           :description="$t('fields.front-end.description')"
+          :image="require('../../../assets/images/fields/front-end.jpg')"
         />
         <FieldsItem
           :title="$t('fields.back-end.title')"
           :description="$t('fields.back-end.description')"
+          :image="require('../../../assets/images/fields/back-end.png')"
         />
         <FieldsItem
           :title="$t('fields.software-architect.title')"
           :description="$t('fields.software-architect.description')"
+          :image="
+            require('../../../assets/images/fields/software-architecture.jpg')
+          "
         />
         <FieldsItem
           :title="$t('fields.RDBMS.title')"
           :description="$t('fields.RDBMS.description')"
+          :image="require('../../../assets/images/fields/database.jpg')"
         />
         <FieldsItem
           :title="$t('fields.start-up-expert.title')"
           :description="$t('fields.start-up-expert.description')"
+          :image="require('../../../assets/images/fields/startup.jpg')"
         />
         <FieldsItem
           :title="$t('fields.microsoft.title')"
           :description="$t('fields.microsoft.description')"
+          :image="require('../../../assets/images/fields/microsoft.jpg')"
         />
       </div>
     </div>
